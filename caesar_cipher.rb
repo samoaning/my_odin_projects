e597e873abbf4767349aeafa6aeceb8c431e4f2f
Steve Yankson Jr <samoaning@gmail.com>
2018-04-03 01:49:21 +0000

    fix spacing

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,16 @@
-=begin
-Author: Steve Yankson
-Project "Caesar Cipher" from The Odin Project
-2nd/April/2018
-=end
-
-
 def caesar_cipher(string, shift_factor)
   @string = string
   @shift_factor = shift_factor.to_i
 
-  encrypt = @string.split('').map! { |v| v.ord  + @shift_factor }
+  encrypt = @string.split('').map! do |v|
+    if v == " "
+      v.ord
+    else
+      v.ord  + @shift_factor
+    end
+  end
   readable_format = encrypt.map! { |k| k.chr }
   return readable_format.join("")
 end
+
+caesar_cipher('good morning hi', 10)
