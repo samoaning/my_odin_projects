b1bbdadf99c7d8f287146d205b1d44d3284f126a
Steve Yankson Jr <samoaning@gmail.com>
2018-06-12 21:46:20 +0000

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,3 +19,30 @@ def bubble_sort(arr)
 end
 
 p bubble_sort(arr)
+
+
+
+arr = ['hi', 'hello', 'hey']
+
+def bubble_sort_by(arr)
+	n = arr.length
+
+	loop do
+		swapped = false
+
+		(n-1).times do |i|
+			difference = yield(arr[i], arr[i+1])
+			if difference > 0 
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				swapped = true 
+			end
+		end
+		break if !swapped
+	end
+
+	p arr
+end
+
+bubble_sort_by(arr) do |left, right|
+	left.length - right.length
+end
